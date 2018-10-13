7add83e3a2853cb7ad7a8e3054bfeffd041f0019
Sandro Sena <sandro340@hotmail.com>
2018-10-13 16:52:38 -0300

    Shit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,10 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import json_data from "./frases.json";
+import Components from "./Components";
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      frase: "..."
-    }
-
-    this.gerarFrase();
-  }
-
-  geraNumeroInteiro(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-
-  mudaFrase = event => {
-    const fraseGenerator = this.todasFrases[this.geraNumeroInteiro(0, 11)];
-    this.setState({
-      frase: fraseGenerator
-    });
-  };
-
-  gerarFrase = () => {
-    const totalFrases = json_data;
-
-    this.todasFrases = totalFrases.map(frases => frases.frase);
-  };
-
   render() {
-    return (
-      <div className="container">
-        <p>{this.state.frase}</p>
-        <button onClick={this.mudaFrase} className="btn">Gerar Frase</button>
-      </div>
-    );
+    return <Components />;
   }
 }
 
---
+++ src/Components.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import json_data from "./frases.json";
+
+class Components extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      frase: "..."
+    };
+
+    this.gerarFrase();
+  }
+
+  geraNumeroInteiro(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  mudaFrase = event => {
+    const fraseGenerator = this.todasFrases[this.geraNumeroInteiro(0, 11)];
+    this.setState({
+      frase: fraseGenerator
+    });
+  };
+
+  gerarFrase = () => {
+    const totalFrases = json_data;
+
+    this.todasFrases = totalFrases.map(frases => frases.frase);
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <p>{this.state.frase}</p>
+        <button onClick={this.mudaFrase} className="btn">Gerar Frase</button>
+      </div>
+    );
+  }
+}
+
+export default Components;
